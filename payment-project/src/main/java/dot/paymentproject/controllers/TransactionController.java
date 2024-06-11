de6dc493c25f0399a0c197a71014afdd1ba3d70e
iuche <111972019+iuche@users.noreply.github.com>
2024-06-11 10:34:32 +0100

    included documentation to the controller

--- payment-project/src/main/java/dot/paymentproject/controllers/TransactionController.java
+++ payment-project/src/main/java/dot/paymentproject/controllers/TransactionController.java
@@ -12,6 +12,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * This is the main controller for handling transaction related requests.
+ * It provides endpoints for initiating outflow and inflow transactions and for fetching transaction logs.
+ */
 @RestController
 @RequestMapping("/api/v1/dot/transaction/")
 @RequiredArgsConstructor
@@ -19,18 +23,36 @@ public class TransactionController {
     private final TransactionInterface transactionInterface;;
     Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
+    /**
+     * This endpoint is used to initiate an outflow transaction.
+     * @param request The request body should contain the details of the outflow transaction.
+     * @return Returns a response entity containing the result of the transaction.
+     */
     @PostMapping("outflow")
     public ResponseEntity<TransferResponse> outflowTransaction(TransferRequest request) {
         logger.info("transaction outflow initiated...");
         return transactionInterface.outflow(request);
     }
 
+    /**
+     * This endpoint is used to initiate an inflow transaction.
+     * @param request The request body should contain the details of the inflow transaction.
+     * @return Returns a response entity containing the result of the transaction.
+     */
     @PostMapping("inflow")
     public ResponseEntity<TransferResponse> inflowTransaction(InflowRequest request){
         logger.info("transaction inflow initiated");
         return transactionInterface.inflow(request);
     }
 
+    /**
+     * This endpoint is used to fetch transaction logs.
+     * @param page The page number for pagination.
+     * @param size The number of records per page for pagination.
+     * @param startDate The start date for the transaction log.
+     * @param endDate The end date for the transaction log.
+     * @return Returns a custom page response containing the transaction logs.
+     */
     @GetMapping("get_transaction_log")
     public CustomPageResponse<TransactionLog> getTransactionLog(@RequestParam("page") int page, @RequestParam("size") int size,
                                                                 @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate){
